eb06d7d093f72a801eae8829610afe0a095f644e
VenkatZen <venkatakrishnan.s@zencode.guru>
2018-12-05 15:13:50 +0530

    js and css

---
+++ main.js
@@ -0,0 +1,43 @@
+/*var hhead=document.querySelector('h1');
+hhead.onclick=display;
+function display()
+{
+    hhead.textContent=welcome;
+    alert('text have been changed');
+}*/
+var myimage=document.querySelector("img");
+myimage.onclick=imagechange;
+function imagechange()
+{
+    var imgsrc=myimage.getAttribute('src');
+    if(imgsrc==='android.png')
+    {
+        myimage.setAttribute('src','bb.jpeg');
+        var para=document.querySelector('p');
+        para.textContent="blackberry";
+    }
+    else{
+        myimage.setAttribute('src','android.png');
+        
+    }
+}
+var mybutton=document.querySelector('button');
+var myheading=document.querySelector('h1');
+function setusername()
+{
+    var uname=prompt("enter your name");
+    localStorage.setItem('name',uname);
+    myheading.textContent="welcome to this web "+uname;
+}
+if(localStorage.getItem('name'))
+{
+   setusername();
+}
+else{
+    var storedname=localStorage.getItem('name');
+    myheading.textContent="welcome to this web "+storedname;
+}
+mybutton.onclick=function()
+{
+    setusername();
+}
