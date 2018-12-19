4a4bb8afad7988ceefc68c916a0afb0ddc9ba449
VenkatZen <venkatakrishnan.s@zencode.guru>
2018-12-19 13:25:56 +0530

    array concept project

--- array_concept/clear.js
+++
@@ -1,7 +0,0 @@
-var emp=[{name:'venkat',age:22,experience:12,city:'madurai',position:'projectmanager'},
-          {name:'krishnan',age:34,experience:15,city:'chennai' position:'projectmanager'},
-        {name:'sparrow',age:18,experience:3,city:'madurai' position:'developer'} ];
-function age()
-{
-  alert('hellow');
-};
